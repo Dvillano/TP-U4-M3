2e96617baaee96a8dd1e21b37c7b834c59930cc0
dvillano <danie.villano@gmail.com>
2021-05-17 18:47:34 -0300

    Added views

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mysql = require('mysql');
 const util = require('util');
+const ejs = require('ejs');
+
 
 const app = express();
 const PORT = 3000;
 
+// Leer archivos estaticos de la carpeta public
 app.use(express.static('public'));
+// Leer info del form
+app.use(express.urlencoded({extended:true}));
+
+// Cargar vista con ejs
+app.set('view engine', 'ejs');
 
 
 app.use(express.json()); // Permite el mapeo de la peticion de JSON a JS
@@ -15,7 +23,7 @@ const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'gestionempleados'
+    database: 'rifa'
 });
 
 // Verificar conexion a base de datos
@@ -31,56 +39,82 @@ const qy = util.promisify(conexion.query).bind(conexion); // Permite el uso de a
 
 // Rutas
 
-// EMPLEADOS
-/** 
- * Metodo POST para enviar informacion
- * Metodo GET para mostrarla
- */
+//Participantes
 
-// Obtener toda la informacion de la tabla empleados
-app.get('/empleados', async (req, res) => {
+// Cargar formulario al entrar a la pagina
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,'.public/index.html'));
+})
+
+// Mostrar resultados de la rifa
+app.get('/rifa', async (req, res) => {
     try {
-        const query = 'SELECT * FROM empleados';
+        const query = 'SELECT * FROM participante';
 
         const respuesta = await qy(query);
 
-        res.send({'respuesta': respuesta});
-        console.log(respuesta);
+        for (let i = 0; i < respuesta.length; i++){
+            if (respuesta[i].id_pais == 1){
+                respuesta[i].id_pais = "Argentina"; 
+            }
+            if (respuesta[i].id_pais == 2){
+                respuesta[i].id_pais = "Uruguay"; 
+            }
+            if (respuesta[i].id_pais == 3){
+                respuesta[i].id_pais = "Brasil"; 
+            }
+        }
+        res.render('pages/resultados', {
+            respuesta
+        })
+
     } catch (error) {
         console.error(error.message);
         res.status(413).send({'Error': error.message});
     }
 })
 
-// Agregar informacion de empleado
-app.post('/empleados', async (req, res) => {
+// Enviar informacion de participante en formulario
+app.post('/rifa', async (req, res) => {
     try {
-        // Validacion
-        if(!req.body.nombre || !req.body.apellido || !req.body.sexo || !req.body.oficina){
-            throw new Error('Falta enviar algun dato');
+
+        console.log("Datos recibidos en formulario: " + req.body.nombre, req.body.apellido, req.body.numero, req.body.pais)
+        if (!req.body.nombre || !req.body.apellido || !req.body.numero || !req.body.pais){
+            throw new Error ("Faltan datos por enviar");
         }
 
-        //Verifico que no exista previamente esa persona 
-        let query = 'SELECT * FROM empleados WHERE nombre = ? AND apellido = ? AND sexo = ?'
+    // Valido que no se repita participante
+    let query = 'SELECT * FROM participante WHERE nombre = ? AND apellido = ?'
+    let respuesta = await qy(query, [req.body.nombre, req.body.apellido]);
 
-        let respuesta = await qy(query, [req.body.nombre, req.body.apellido, req.body.sexo]);
+    if (respuesta.length > 0){
+        throw new Error('Ya elegiste tu numero de rifa');
+    }
 
-        if (respuesta.length > 0){
-            throw new Error('Ese empleado ya existe');
-        }
+    // Valido que no se repita numero
+    query = 'SELECT * FROM participante WHERE numero = ?'
+    respuesta = await qy(query, [req.body.numero]);
 
-        // Guardar empleado
-        query = 'INSERT INTO empleados (nombre, apellido, sexo, oficina) VALUES (?, ?, ?)';
-        respuesta = await qy(query, [req.body.nombre, req.body.apellido, req.body.sexo]);    
-        
-        console.log(respuesta);
-        res.send({'respuesta': respuesta.affectedRows});
+    if (respuesta.length > 0){
+        throw new Error('Ya este numero esta tomado, por favor elegir otro');
+    }
+
+    // Guardo el participante y su eleccion
+    query = 'INSERT INTO participante (nombre, apellido, numero, id_pais) VALUE (?, ?, ?, ?)';
+    respuesta = await qy(query, [req.body.nombre, req.body.apellido, req.body.numero, req.body.pais]);
+
+    res.redirect('/rifa');
 
     } catch (error) {
         console.error(error.message);
         res.status(413).send({'Error': error.message});
     }
 })
+
+
+app.get("*", (req,res) => {
+    res.redirect("/");
+}) 
 
 
 app.listen(PORT, () => {
